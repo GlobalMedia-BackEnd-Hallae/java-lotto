929667e5d3af4fc2cb79e5e9ff8099d65e6b4e8e
Sy P <yongsa0221@gmail.com>
2023-01-27 05:29:45 +0900

    feat : Lotto 클래스 추가
    - 로또 번호 객체 구현

---
+++ src/main/java/gmbs/Lotto.java
@@ -0,0 +1,36 @@
+package gmbs;
+
+import java.util.*;
+
+public class Lotto {
+
+    private final Set<Integer> numbers;
+
+    public Lotto(LottoNumbers numbers) {
+        this.numbers = numbers.getNumbers();
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Lotto lotto = (Lotto) o;
+        return equalsSet(numbers, lotto.numbers);
+    }
+
+    private boolean equalsSet(Set<Integer> set1, Set<Integer> set2) {
+        return set1 != null
+                && set2 != null
+                && set1.size() == set2.size()
+                && set1.containsAll(set2);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numbers);
+    }
+
+    public Set<Integer> getNumber() {
+        return numbers;
+    }
+}
