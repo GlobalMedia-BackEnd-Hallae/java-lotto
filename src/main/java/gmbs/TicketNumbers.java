aa74ac0a9a2eac7c3f485a15906574ffde2456b6
Sy P <yongsa0221@gmail.com>
2023-01-27 05:02:01 +0900

    feat : ticketNumbers 클래스 추가
    - 난수 생성

---
+++ src/main/java/gmbs/TicketNumbers.java
@@ -0,0 +1,21 @@
+package gmbs;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class TicketNumbers implements LottoNumbers {
+
+    private final Set<Integer> lottoNumbers = new HashSet<>();
+
+    public TicketNumbers() {
+        Random random = new Random();
+        while(lottoNumbers.size() <= VALID_LENGTH) {
+            lottoNumbers.add(random.nextInt(MAX - MIN + 1) + MIN);
+        }
+    }
+    @Override
+    public Set<Integer> getNumbers() {
+        return lottoNumbers;
+    }
+}
