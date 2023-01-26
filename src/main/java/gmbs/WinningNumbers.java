0ed2ffac28ced96fc6115ad5b9e261ee0285e28f
Sy P <yongsa0221@gmail.com>
2023-01-27 03:54:41 +0900

    feat : WinningNumbers 클래스 추가
    - 길이 검증
    - 숫자 여부 검증
    - 중복 검증
    - 숫자 범위 검증

---
+++ src/main/java/gmbs/WinningNumbers.java
@@ -0,0 +1,54 @@
+package gmbs;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class WinningNumbers implements LottoNumbers {
+
+    private final Set<Integer> lottoNumbers;
+
+    public WinningNumbers(List<String> userInputNumbers) {
+        Set<Integer> numbers = convert(userInputNumbers);
+        lengthValidate(numbers);
+        overlapValidate(userInputNumbers,numbers);
+        rangeValidate(numbers);
+        lottoNumbers = numbers;
+    }
+
+    private void lengthValidate(Set<Integer> userInputNumbers) {
+        if (userInputNumbers.size() != VALID_LENGTH) {
+            throw new IllegalArgumentException("[error] invalid length");
+        }
+    }
+
+    private void typeValidate(List<String> userInputNumbers) {
+        if (userInputNumbers.stream().allMatch(number -> number.matches("^[0-9]*$"))) {
+            throw new IllegalArgumentException("[error] is not number");
+        }
+    }
+
+    private void overlapValidate(List<String> userInputNumbers, Set<Integer> convertNumber) {
+        if (userInputNumbers.size() != convertNumber.size()) {
+            throw new IllegalArgumentException("[error] has overlap");
+        }
+    }
+
+    private Set<Integer> convert(List<String> userInputNumbers) {
+        typeValidate(userInputNumbers);
+        return userInputNumbers.stream()
+                .map(Integer::parseInt)
+                .collect(Collectors.toSet());
+    }
+
+    private void rangeValidate(Set<Integer> numbers) {
+        if (numbers.stream().anyMatch(number -> number < MIN || number > MAX)) {
+            throw new IllegalArgumentException("[error] invalid number range");
+        }
+    }
+
+    @Override
+    public Set<Integer> getNumbers() {
+        return lottoNumbers;
+    }
+}
