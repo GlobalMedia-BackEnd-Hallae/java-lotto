e54f4d1440b9b6c36a3d3c4f0795168f830b2a9e
chanwoo <chocw0402@gmail.com>
2023-01-28 23:26:13 +0900

    feat : InputConsole 인터페이스 개발

---
+++ src/main/java/gmbs/view/input/InputConsole.java
@@ -0,0 +1,12 @@
+package gmbs.view.input;
+
+import java.util.List;
+
+public interface InputConsole {
+
+    String readBuyAmount();
+
+    List<Integer> readWinNumbers();
+
+    int readBonusNumber();
+}
