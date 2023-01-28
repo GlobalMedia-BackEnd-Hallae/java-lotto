ffffd5e380b15005f1d60e7ef7934ef0630ddb03
Sy P <yongsa0221@gmail.com>
2023-01-29 06:43:49 +0900

    feat : main 기능 구현

--- src/main/java/gmbs/Main.java
+++ src/main/java/gmbs/Main.java
@@ -1,6 +1,10 @@
 package gmbs;
 
+import gmbs.controller.LottoController;
+
 public class Main {
     public static void main(String[] args) {
+        LottoController controller = new LottoController();
+        controller.operate();
     }
 }
