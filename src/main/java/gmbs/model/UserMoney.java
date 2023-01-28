8287694b1d4ce11bf642b9e6e309b4865b3352f2
Sy P <yongsa0221@gmail.com>
2023-01-29 03:23:32 +0900

    feat : userMoney 클래스 생성

---
+++ src/main/java/gmbs/model/UserMoney.java
@@ -0,0 +1,30 @@
+package gmbs.model;
+
+public class UserMoney {
+
+    private static final int DEFAULT_TICKET_PRICE = 1000;
+    private final int money;
+
+    public UserMoney(String userInput) {
+        typeValidate(userInput);
+        int convertMoney = Integer.parseInt(userInput);
+        moneyValidate(convertMoney);
+        this.money = convertMoney;
+    }
+
+    private void typeValidate(String number) {
+        if (!number.matches("^[0-9]*$")) {
+            throw new IllegalArgumentException("[error] is not number");
+        }
+    }
+
+    private void moneyValidate(int money) {
+        if (money % DEFAULT_TICKET_PRICE != 0 || money == 0) {
+            throw new IllegalArgumentException("[error] invalid money input");
+        }
+    }
+
+    public int getTicketCount() {
+        return money / DEFAULT_TICKET_PRICE;
+    }
+}
---
+++ src/test/java/gmbs/model/UserMoneyTest.java
@@ -0,0 +1,38 @@
+package gmbs.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+class UserMoneyTest {
+
+    @ParameterizedTest
+    @DisplayName("자연수가 아니면 예외 발생한다")
+    @ValueSource(strings = {"a", "-1000", "1.4"})
+    void exceptionByInvalidExpression(String input) {
+        assertThatThrownBy(() -> new UserMoney(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[error] is not number");
+    }
+
+    @ParameterizedTest
+    @DisplayName("1000단위로 나뉘지 않으면 예외 발생한다")
+    @ValueSource(strings = {"100", "0", "1401"})
+    void exceptionByInvalidMoneyInput(String input) {
+        assertThatThrownBy(() -> new UserMoney(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[error] invalid money input");
+    }
+
+    @Test
+    @DisplayName("살 수 있는 로또 티켓 수를 반환한다")
+    void testGetTicketCount() {
+        UserMoney money = new UserMoney("14000");
+        int expected = 14;
+        assertThat(money.getTicketCount()).isEqualTo(expected);
+    }
+}
