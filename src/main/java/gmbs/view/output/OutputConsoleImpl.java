70a37f43aff32070dffe55884b8c3af1731f5e7d
chanwoo <chocw0402@gmail.com>
2023-01-28 23:27:02 +0900

    feat : OutputConsole 인터페이스 구현

---
+++ src/main/java/gmbs/view/output/OutputConsoleImpl.java
@@ -0,0 +1,38 @@
+package gmbs.view.output;
+
+import gmbs.model.inner.lotto.result.LottoResult;
+import gmbs.model.inner.lotto.ticket.LottoTickets;
+
+public class OutputConsoleImpl implements OutputConsole {
+
+    private static final String LINE_BREAK = "\n";
+
+    @Override
+    public void printAutoLottoTickets(LottoTickets lottoTickets) {
+        StringBuilder ticketResult = new StringBuilder();
+        ticketResult.append(lottoTickets.getQuantity()).append("개를 구매했습니다").append(LINE_BREAK);
+        lottoTickets.getValues().forEach(lottoTicket -> ticketResult
+                .append(lottoTicket.getLottoNumbers())
+                .append(LINE_BREAK)
+        );
+        System.out.println(ticketResult);
+    }
+
+    @Override
+    public void printWinningStatistics(LottoResult lottoResult) {
+        StringBuilder statisticsResult = new StringBuilder();
+        statisticsResult.append("당첨 통계").append(LINE_BREAK).append("---").append(LINE_BREAK);
+        lottoResult.getRankResults().forEach((rank, countOfRank) -> statisticsResult
+                .append(rank.getDisplay())
+                .append("(")
+                .append(rank.getWinningPrize())
+                .append(")원")
+                .append(" - ")
+                .append(countOfRank)
+                .append("개")
+                .append(LINE_BREAK)
+        );
+        statisticsResult.append("총 수익률은 ").append(lottoResult.getRateOfReturn()).append("% 입니다");
+        System.out.println(statisticsResult);
+    }
+}
