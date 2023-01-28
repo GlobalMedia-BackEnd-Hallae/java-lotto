0fe08d817850d9bf3e817f231e13b0443db63f9b
chanwoo <chocw0402@gmail.com>
2023-01-28 23:03:06 +0900

    test : LottoNumberGeneratorImpl 테스트

---
+++ src/test/java/gmbs/model/inner/lotto/number/impl/LottoNumberGeneratorImplTest.java
@@ -0,0 +1,42 @@
+package gmbs.model.inner.lotto.number.impl;
+
+import gmbs.model.inner.lotto.number.LottoNumberGenerator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LottoNumberGeneratorImplTest {
+
+    private static final int MAX_LOTTO_SIZE = 6;
+    private static final int[] CONTAIN_DUPLICATE_NUMBERS = new int[] {1, 1, 2, 3, 4, 5, 6};
+    private static final int[] NON_DUPLICATE_NUMBERS = new int[] {1, 2, 3, 4, 5, 6};
+
+    private int index = 0;
+
+    @DisplayName("중복이 없는 여섯개의 숫자 리스트를 반환한다")
+    @ParameterizedTest
+    @MethodSource("providerRandomNumbers")
+    void generate(int[] randomNumbers, int expectSize) {
+        // given
+        LottoNumberGenerator lottoNumberGenerator = new LottoNumberGeneratorImpl(() -> randomNumbers[index++]);
+
+        // when
+        List<Integer> actualLottoNumbers = lottoNumberGenerator.generate();
+
+        // then
+        assertThat(actualLottoNumbers).hasSize(expectSize);
+    }
+
+    private static Stream<Arguments> providerRandomNumbers() {
+        return Stream.of(
+                Arguments.of((Object) CONTAIN_DUPLICATE_NUMBERS, MAX_LOTTO_SIZE),
+                Arguments.of((Object) NON_DUPLICATE_NUMBERS, MAX_LOTTO_SIZE)
+        );
+    }
+}
