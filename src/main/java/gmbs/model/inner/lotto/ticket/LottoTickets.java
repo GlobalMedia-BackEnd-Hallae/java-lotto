49ea5c1942376974fe3ee5b0fdc531c183ccded0
chanwoo <chocw0402@gmail.com>
2023-01-28 23:09:03 +0900

    feat : LottoTickets 객체 개발 - LottoTicket 을 구매 수량만큼 가지고 있는 일급 컬렉션 객체

---
+++ src/main/java/gmbs/model/inner/lotto/ticket/LottoTickets.java
@@ -0,0 +1,49 @@
+package gmbs.model.inner.lotto.ticket;
+
+import gmbs.model.dto.NumberDto;
+import gmbs.model.inner.dto.MatchResultDto;
+import gmbs.model.inner.lotto.number.LottoNumberGenerator;
+import gmbs.model.inner.lotto.number.impl.LottoNumberGeneratorImpl;
+import gmbs.model.inner.lotto.number.RandomNumber;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LottoTickets {
+
+    private final LottoNumberGenerator lottoNumberGenerator;
+    private final List<LottoTicket> values;
+    private final Long quantity;
+
+    public LottoTickets(final RandomNumber randomNumber, final Long buyQuantity) {
+        this.lottoNumberGenerator = new LottoNumberGeneratorImpl(randomNumber);
+        this.values = issueLottoTickets(buyQuantity);
+        this.quantity = buyQuantity;
+    }
+
+    private List<LottoTicket> issueLottoTickets(Long buyQuantity) {
+        final List<LottoTicket> lottoTickets = new ArrayList<>();
+        while (lottoTickets.size() < buyQuantity) {
+            lottoTickets.add(new LottoTicket(lottoNumberGenerator));
+        }
+        return lottoTickets;
+    }
+
+    public List<MatchResultDto> aggregateMatchResults(NumberDto numberDto) {
+        return values.stream()
+                .map(lottoTicket -> MatchResultDto.of(
+                        lottoTicket.hasBonus(numberDto.getBonusNumber()),
+                        lottoTicket.calculateMatchCountByWinNumbers(numberDto.getWinNumbers())
+                ))
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    public List<LottoTicket> getValues() {
+        return values;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+}
