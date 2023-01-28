bbb6a9d689c9d1d6676a723b41e268dd4aa0c4c5
chanwoo <chocw0402@gmail.com>
2023-01-28 23:30:55 +0900

    feat : Main 실행 클래스

--- src/main/java/gmbs/Main.java
+++ src/main/java/gmbs/Main.java
@@ -1,6 +1,17 @@
 package gmbs;
 
+import gmbs.controller.LottoController;
+import gmbs.view.input.InputConsole;
+import gmbs.view.input.InputConsoleImpl;
+import gmbs.view.output.OutputConsole;
+import gmbs.view.output.OutputConsoleImpl;
+
 public class Main {
+
     public static void main(String[] args) {
+        InputConsole inputConsole = new InputConsoleImpl();
+        OutputConsole outputConsole = new OutputConsoleImpl();
+        LottoController lottoController = new LottoController(inputConsole, outputConsole);
+        lottoController.run();
     }
 }
