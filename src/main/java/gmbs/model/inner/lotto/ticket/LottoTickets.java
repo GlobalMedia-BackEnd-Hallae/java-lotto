16a66f387db4b14a332a2aaa35f68904f83d6ada
chanwoo <chocw0402@gmail.com>
2023-01-29 01:03:42 +0900

    refactor : MatchResultDto 의 생성자 파라미터 순서 변경에 따른aggregateMatchResults 메서드 리팩토링

--- src/main/java/gmbs/model/inner/lotto/ticket/LottoTickets.java
+++ src/main/java/gmbs/model/inner/lotto/ticket/LottoTickets.java
@@ -33,8 +33,8 @@ public class LottoTickets {
     public List<MatchResultDto> aggregateMatchResults(NumberDto numberDto) {
         return values.stream()
                 .map(lottoTicket -> MatchResultDto.of(
-                        lottoTicket.hasBonus(numberDto.getBonusNumber()),
-                        lottoTicket.calculateMatchCountByWinNumbers(numberDto.getWinNumbers())
+                        lottoTicket.calculateMatchCountByWinNumbers(numberDto.getWinNumbers()),
+                        lottoTicket.hasBonus(numberDto.getBonusNumber())
                 ))
                 .collect(Collectors.toUnmodifiableList());
     }
