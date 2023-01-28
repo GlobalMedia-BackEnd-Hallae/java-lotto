df75a3e4aa1f4b382bd8fb650476472e01cde284
chanwoo <chocw0402@gmail.com>
2023-01-29 01:02:43 +0900

    refactor : 생성자 파라미터 순서 변경

--- src/main/java/gmbs/model/inner/dto/MatchResultDto.java
+++ src/main/java/gmbs/model/inner/dto/MatchResultDto.java
@@ -2,23 +2,23 @@ package gmbs.model.inner.dto;
 
 public class MatchResultDto {
 
-    private final boolean hasBonus;
     private final int matchCount;
+    private final boolean hasBonus;
 
-    private MatchResultDto(final boolean hasBonus, final int matchCount) {
-        this.hasBonus = hasBonus;
+    private MatchResultDto(final int matchCount, final boolean hasBonus) {
         this.matchCount = matchCount;
+        this.hasBonus = hasBonus;
     }
 
-    public static MatchResultDto of(final boolean hasBonus, final int matchCount) {
-        return new MatchResultDto(hasBonus, matchCount);
-    }
-
-    public boolean getHasBonus() {
-        return hasBonus;
+    public static MatchResultDto of(final int matchCount, final boolean hasBonus) {
+        return new MatchResultDto(matchCount, hasBonus);
     }
 
     public int getMatchCount() {
         return matchCount;
+    }
+
+    public boolean getHasBonus() {
+        return hasBonus;
     }
 }
