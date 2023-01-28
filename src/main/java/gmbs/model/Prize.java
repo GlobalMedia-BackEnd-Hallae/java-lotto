5f0b7b5f0b590dc5ca9021bfcf585ca24ce13142
Sy P <yongsa0221@gmail.com>
2023-01-29 05:53:48 +0900

    feat : prizeEnum 값 추가

--- src/main/java/gmbs/model/Prize.java
+++ src/main/java/gmbs/model/Prize.java
@@ -1,20 +1,32 @@
 package gmbs.model;
 
 public enum Prize {
-    FIRST(2000000000),
-    SECOND(30000000),
-    THIRD(1500000),
-    FOURTH(50000),
-    FIFTH(5000),
-    LOSER(0);
+    FIRST(2000000000, 1, 6),
+    SECOND(30000000, 2, 5),
+    THIRD(1500000, 3, 5),
+    FOURTH(50000, 4, 4),
+    FIFTH(5000, 5, 3),
+    LOSER(0, 6, 2);
 
     private final int money;
+    private final int place;
+    private final int matches;
 
-    Prize(int money) {
+    Prize(int money, int place, int matches) {
         this.money = money;
+        this.place = place;
+        this.matches = matches;
     }
 
     public int money() {
         return money;
+    }
+
+    public int place() {
+        return place;
+    }
+
+    public int matches() {
+        return matches;
     }
 }
