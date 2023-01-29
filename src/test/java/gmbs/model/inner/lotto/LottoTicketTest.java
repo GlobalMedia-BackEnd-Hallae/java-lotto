bf9382a208125b4646fca1be14ad03d55fcaca74
chanwoo <chocw0402@gmail.com>
2023-01-29 23:03:12 +0900

    refactor : LottoTicket 테스트 리팩토링

--- src/test/java/gmbs/model/inner/lotto/LottoTicketTest.java
+++ src/test/java/gmbs/model/inner/lotto/LottoTicketTest.java
@@ -1,7 +1,5 @@
-package gmbs.model.inner.lotto.ticket;
+package gmbs.model.inner.lotto;
 
-import gmbs.model.inner.lotto.number.LottoNumberGenerator;
-import gmbs.model.inner.lotto.number.impl.LottoNumberGeneratorImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -14,20 +12,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoTicketTest {
 
-    private static final int INIT_INDEX = 0;
-    private static final int BONUS_NUMBER = 7;
-    private static final int[] GENERATED_NUMBER_CONTAIN_BONUS = new int[] {1, 2, 3, 4, 5, BONUS_NUMBER};
-    private static final int[] GENERATED_NUMBER_NOT_CONTAIN_BONUS = new int[] {1, 2, 3, 4, 5, BONUS_NUMBER - 1};
-
-    private int index = 0;
-
     @DisplayName("자동 생성된 로또 번호와 보너스 번호를 받아, 보너스 번호가 있다면 true, 없다면 false 를 반환한다")
     @ParameterizedTest
     @MethodSource("providerGeneratedRandomNumbersAndBonusNumberAndExpect")
-    void hasBonus(int[] generateRandomNumbers, int bonusNumber, boolean expect) {
+    void hasBonus(List<Integer> generateRandomNumbers, int bonusNumber, boolean expect) {
         // given
-        LottoNumberGenerator lottoNumberGenerator = new LottoNumberGeneratorImpl(() -> generateRandomNumbers[index++]);
-        LottoTicket lottoTicket = new LottoTicket(lottoNumberGenerator);
+        LottoTicket lottoTicket = new LottoTicket(generateRandomNumbers);
 
         // when
         boolean actual = lottoTicket.hasBonus(bonusNumber);
@@ -38,19 +28,17 @@ class LottoTicketTest {
 
     private static Stream<Arguments> providerGeneratedRandomNumbersAndBonusNumberAndExpect() {
         return Stream.of(
-                Arguments.of((Object) GENERATED_NUMBER_CONTAIN_BONUS, BONUS_NUMBER, true),
-                Arguments.of((Object) GENERATED_NUMBER_NOT_CONTAIN_BONUS, BONUS_NUMBER, false)
+                Arguments.of(List.of(1, 2, 3, 4, 5, 7), 7, true),
+                Arguments.of(List.of(1, 2, 3, 4, 5, 6), 7, false)
         );
     }
 
     @DisplayName("자동 생성된 로또 번호와 당첨 번호를 받아, 당첨 번호와 일치하는 번호 갯수를 반환한다")
     @ParameterizedTest
     @MethodSource("providerGeneratedRandomNumbersAndWinNumbersAndExpectSize")
-    void calculateMatchCountByWinNumbers(int[] generateRandomNumbers, List<Integer> winNumbers, int expectSize) {
+    void calculateMatchCountByWinNumbers(List<Integer> generateRandomNumbers, List<Integer> winNumbers, int expectSize) {
         // given
-        index = INIT_INDEX;
-        LottoNumberGenerator lottoNumberGenerator = new LottoNumberGeneratorImpl(() -> generateRandomNumbers[index++]);
-        LottoTicket lottoTicket = new LottoTicket(lottoNumberGenerator);
+        LottoTicket lottoTicket = new LottoTicket(generateRandomNumbers);
 
         // when
         int actualSize = lottoTicket.calculateMatchCountByWinNumbers(winNumbers);
@@ -61,12 +49,11 @@ class LottoTicketTest {
 
     private static Stream<Arguments> providerGeneratedRandomNumbersAndWinNumbersAndExpectSize() {
         return Stream.of(
-                Arguments.of((Object) GENERATED_NUMBER_NOT_CONTAIN_BONUS, List.of(1, 2, 3, 4, 5, 6), 6),
-                Arguments.of((Object) GENERATED_NUMBER_CONTAIN_BONUS, List.of(1, 2, 3, 4, 5, 6), 5),
-                Arguments.of((Object) GENERATED_NUMBER_NOT_CONTAIN_BONUS, List.of(1, 2, 3, 4, 5, 45), 5),
-                Arguments.of((Object) GENERATED_NUMBER_NOT_CONTAIN_BONUS, List.of(1, 2, 3, 4, 44, 45), 4),
-                Arguments.of((Object) GENERATED_NUMBER_NOT_CONTAIN_BONUS, List.of(1, 2, 3, 43, 44, 45), 3),
-                Arguments.of((Object) GENERATED_NUMBER_NOT_CONTAIN_BONUS, List.of(40, 41, 42, 43, 44, 45), 0)
+                Arguments.of(List.of(1, 2, 3, 4, 5, 6), List.of(1, 2, 3, 4, 5, 6), 6),
+                Arguments.of(List.of(1, 2, 3, 4, 5, 7), List.of(1, 2, 3, 4, 5, 45), 5),
+                Arguments.of(List.of(1, 2, 3, 4, 5, 7), List.of(1, 2, 3, 4, 44, 45), 4),
+                Arguments.of(List.of(1, 2, 3, 4, 5, 7), List.of(1, 2, 3, 43, 44, 45), 3),
+                Arguments.of(List.of(1, 2, 3, 4, 5, 7), List.of(40, 41, 42, 43, 44, 45), 0)
         );
     }
 }
