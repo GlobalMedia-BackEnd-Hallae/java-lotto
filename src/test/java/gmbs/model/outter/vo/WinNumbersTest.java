1f1b35f1707e8dd4e3108a2e34a1208ea0a3b0f2
chanwoo <chocw0402@gmail.com>
2023-01-29 22:56:31 +0900

    refactor : WinNumber 테스트 코드 리팩토링 - 상수 필드 이동 및 제거, 단순 getter 테스트 제거, 객체 생성 성공 테스트 변경

--- src/test/java/gmbs/model/outter/vo/WinNumbersTest.java
+++ src/test/java/gmbs/model/outter/vo/WinNumbersTest.java
@@ -9,19 +9,12 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.List;
 import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.assertj.core.api.Assertions.assertThat;
 
 class WinNumbersTest {
 
-    private static final int MIN_WIN_NUMBER = 1;
-    private static final int MAX_WIN_NUMBER = 45;
-    private static final int UNDER_MIN_WIN_NUMBER = MIN_WIN_NUMBER - 1;
-    private static final int OVER_MAX_WIN_NUMBER = MAX_WIN_NUMBER + 1;
-    private static final List<Integer> WIN_NUMBERS = List.of(MIN_WIN_NUMBER, 2, 3, 4, 5, MAX_WIN_NUMBER);
-
-    private WinNumbers winNumbers;
+    private static final List<Integer> WIN_NUMBERS = List.of(1, 2, 3, 4, 5, 6);
 
     @DisplayName("winNumbers 가 6개가 아닐 시 예외를 발생시킨다")
     @ParameterizedTest
@@ -72,42 +65,23 @@ class WinNumbersTest {
 
     private static Stream<Arguments> providerOutOfRangeWinNumbers() {
         return Stream.of(
-                Arguments.of(List.of(UNDER_MIN_WIN_NUMBER, 2, 3, 4, 5, 6)),
-                Arguments.of(List.of(1, 2, 3, 4, 5, OVER_MAX_WIN_NUMBER))
+                Arguments.of(List.of(0, 2, 3, 4, 5, 6)),
+                Arguments.of(List.of(1, 2, 3, 4, 5, 46))
         );
     }
 
-    @DisplayName("1~45 사이의 숫자 6개를 받아 WinNumbers 객체를 생성한다")
+    @DisplayName("1~45 사이의 숫자 6개를 받아 WinNumbers 객체 생성에 성공한다")
     @Test
     void staticFactoryMethodCreateSuccess() {
-        // when
-        winNumbers = WinNumbers.from(WIN_NUMBERS);
-
-        // then
-        assertThat(winNumbers.getClass()).isEqualTo(WinNumbers.class);
-    }
-
-    @DisplayName("당첨 번호들을 가져온다")
-    @Test
-    void getValues() {
-        // given
-        winNumbers = WinNumbers.from(WIN_NUMBERS);
-
-        // when
-        List<Integer> actualWinNumbers = winNumbers.getValues();
-
-        // then
-        assertAll(
-                () -> assertThat(actualWinNumbers).hasSize(WIN_NUMBERS.size()),
-                () -> assertThat(actualWinNumbers).containsAll(WIN_NUMBERS)
-        );
+        // when, then
+        assertThatCode(() -> WinNumbers.from(WIN_NUMBERS)).doesNotThrowAnyException();
     }
 
     @DisplayName("당첨 번호들이 같으면 같은 객체이다")
     @Test
     void equals() {
         // given
-        winNumbers = WinNumbers.from(WIN_NUMBERS);
+        WinNumbers winNumbers = WinNumbers.from(WIN_NUMBERS);
         WinNumbers another = WinNumbers.from(WIN_NUMBERS);
 
         // when
