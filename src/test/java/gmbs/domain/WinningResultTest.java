e82cdb309e82f4d1de8899092f655c0fd6e32317
JunSeop Kim <wnqtjq8958@naver.com>
2023-01-30 00:01:53 +0900

    test : WinningResultTest 기능 테스트

---
+++ src/test/java/gmbs/domain/WinningResultTest.java
@@ -0,0 +1,28 @@
+package gmbs.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class WinningResultTest {
+    @Test
+    @DisplayName("상금합 구하기")
+    void calculate() {
+        // given
+        List<Ranking> lottoNumbersList = new ArrayList<>();
+        lottoNumbersList.add(Ranking.FIFTH);
+        lottoNumbersList.add(Ranking.FIFTH);
+        lottoNumbersList.add(Ranking.FOURTH);
+
+        // when
+
+        // then
+        int expect = Ranking.FIFTH.getPrize() * 2 + Ranking.FOURTH.getPrize();
+        WinningResult winningResult = new WinningResult(lottoNumbersList);
+        assertThat(winningResult.calculatePrizeSum()).isEqualTo(expect);
+    }
+}
