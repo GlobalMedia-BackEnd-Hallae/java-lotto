7ae12415158a0581c704ec17dd524dab72a6965d
JunSeop Kim <wnqtjq8958@naver.com>
2023-01-29 23:57:55 +0900

    test : LottoNumbers 테스트

---
+++ src/test/java/gmbs/domain/LottoNumbersTest.java
@@ -0,0 +1,39 @@
+package gmbs.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LottoNumbersTest {
+
+    @Test
+    @DisplayName("중복된 숫자로 생성시 예외 발생")
+    void incorrect() {
+        assertThatThrownBy(() -> new LottoNumbers("2,2,3,4,5,6")).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("로또 개수는 중복이 불가능합니다.");
+    }
+
+    @Test
+    @DisplayName("숫자가 6개 미만")
+    void incorrect4() {
+        assertThatThrownBy(() -> new LottoNumbers("2,5,6")).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("로또 개수는 6개로 제한됩니다.");
+    }
+
+    @Test
+    @DisplayName("문자열이 포함된 경우")
+    void incorrect2() {
+        assertThatThrownBy(() -> new LottoNumbers("1,aa,3,4,5,6")).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("equals 확인")
+    void test() {
+        LottoNumbers lottoNumbers1 = new LottoNumbers("1,2,3,4,5,6");
+        LottoNumbers lottoNumbers2 = new LottoNumbers("5,2,1,3,4,6");
+
+        assertThat(lottoNumbers1).isEqualTo(lottoNumbers2);
+    }
+}
