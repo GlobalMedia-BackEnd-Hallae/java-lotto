2a542248b26cd0591702cc473c85f8207d023adc
JunSeop Kim <wnqtjq8958@naver.com>
2023-01-29 23:59:34 +0900

    feat : main 실행 클래스

--- src/main/java/gmbs/Main.java
+++ src/main/java/gmbs/Main.java
@@ -1,6 +1,10 @@
 package gmbs;
 
+import gmbs.controller.LottoController;
+
 public class Main {
     public static void main(String[] args) {
+        LottoController lottoController = new LottoController();
+        lottoController.start();
     }
 }
