30426adf4b2f949d5b6253615976a9cc09c8e2e5
Chan531 <chan657312@gmail.com>
2023-01-30 17:13:08 +0900

    test : 구매한 개수만큼 로또 생성 테스트

---
+++ src/test/java/model/LottoCreationTest.java
@@ -0,0 +1,26 @@
+package model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class LottoCreationTest {
+
+    private final LottoCreation lottoCreation = new LottoCreation();
+
+    @Test
+    @DisplayName("전달받은 개수만큼 복권을 생성할 수 있다.")
+    void canCreateLotto() {
+        // given
+        int number = 10;
+
+        // when
+        List<Lotto> lotto = lottoCreation.createLotto(number);
+
+        // then
+        assertThat(lotto.size()).isEqualTo(number);
+    }
+}
