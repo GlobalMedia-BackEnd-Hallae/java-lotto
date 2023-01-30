863cc0f705b77a9f5b6dd98070b4047f007b4ce8
Chan531 <chan657312@gmail.com>
2023-01-31 00:33:09 +0900

    feat : 수익률 계산기

---
+++ src/main/java/model/EarningsRateCalculator.java
@@ -0,0 +1,31 @@
+package model;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class EarningsRateCalculator {
+
+    private final String earningsRate;
+
+    public EarningsRateCalculator(Money money, List<Integer> winningResult) {
+        final List<Long> prize = prizeOfWinning();
+        long prizeSum = 0;
+
+        for (int index = 0; index < prize.size(); index++) {
+            prizeSum += prize.get(index) * winningResult.get(index);
+        }
+
+        this.earningsRate = String.format("%.2f", (double)prizeSum / (double)money.getMoney() * 100);
+    }
+
+    public String getEarningsRate() {
+        return this.earningsRate;
+    }
+
+    private List<Long> prizeOfWinning() {
+        return Stream.of(Winning.values())
+                .map(m -> m.getPrize())
+                .collect(Collectors.toList());
+    }
+}
