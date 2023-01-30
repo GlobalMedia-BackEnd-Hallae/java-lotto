502fae10ea4846b56a1b70a85377c71ecc025475
Chan531 <chan657312@gmail.com>
2023-01-30 18:54:07 +0900

    feat : 로또 추첨

---
+++ src/main/java/model/LotteryDrawing.java
@@ -0,0 +1,97 @@
+package model;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class LotteryDrawing {
+
+    private static final int MATCH_FIVE_NUMBERS = 5;
+    private static final int FAIL_TO_WIN = 5;
+
+    // 당첨 번호와 로또들 비교
+    public List<Integer> drawLottery(Lotto winningNumbers, List<Lotto> createdLotto) {
+        final List<Integer> winningResult = Arrays.asList(0, 0, 0, 0, 0, 0);
+        final List<Integer> winningInformation = getWinningCount();
+
+        for (Lotto lotto : createdLotto) {
+            int count = compareAllLotto(winningNumbers, lotto);
+            count += checkSecondOrThirdPlace(winningNumbers.getBonusNumber(), lotto, count);
+            int index = setIndex(count, winningInformation);
+            winningResult.set(index, winningResult.get(index) + 1);
+        }
+
+        return winningResult;
+    }
+
+    private List<Integer> getWinningCount() {
+        return Stream.of(Winning.values())
+                .map(m -> m.getCount())
+                .collect(Collectors.toList());
+    }
+
+    // 당첨 번호들과 로또 번호들 비교
+    private int compareAllLotto(Lotto winningNumbers, Lotto lotto) {
+        int count = 0;
+
+        for (int lottoNumber : lotto.getLotto()) {
+            count += compareLotto(winningNumbers, lottoNumber);
+        }
+
+        return count;
+    }
+
+    // 당첨 번호들과 로또 번호 비교
+    private int compareLotto(Lotto winningNumbers, int lottoNumber) {
+        int count = 0;
+
+        for (int winningNumber : winningNumbers.getLotto()) {
+            count += compareNumber(winningNumber, lottoNumber);
+        }
+
+        return count;
+    }
+
+    // 2, 3등을 가리기 위해 보너스 번호와 로또 번호 비교, 공백 제외 8줄
+    private int checkSecondOrThirdPlace(int bonusNumber, Lotto lotto, int count) {
+        if (count != MATCH_FIVE_NUMBERS) {
+            return 0;
+        }
+
+        int check = 0;
+
+        for (int lottoNumber : lotto.getLotto()) {
+            check += compareNumber(bonusNumber, lottoNumber);
+        }
+
+        return check;
+    }
+
+    // 당첨 번호와 로또 번호 비교
+    private int compareNumber(int winningNumber, int lottoNumber) {
+        if (winningNumber == lottoNumber) {
+            return 1;
+        }
+
+        return 0;
+    }
+
+    private int setIndex(int count, List<Integer> winningInformation) {
+        int index = FAIL_TO_WIN;
+
+        for (int order = 0; order < winningInformation.size(); order++) {
+            index = compareCount(winningInformation.get(order), count, order);
+        }
+
+        return index;
+    }
+
+    private int compareCount(int winningCount, int count, int order) {
+        if (winningCount == count) {
+            return order;
+        }
+
+        return FAIL_TO_WIN;
+    }
+}
