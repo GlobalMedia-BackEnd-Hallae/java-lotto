135f9770368224341479a22acd99c1d7eb605b4a
Chan531 <chan657312@gmail.com>
2023-01-31 01:09:27 +0900

    fix : 누락된 보너스 번호 입력 멘트 추가

--- src/main/java/view/BonusNumberInput.java
+++ src/main/java/view/BonusNumberInput.java
@@ -7,6 +7,7 @@ public class BonusNumberInput {
     private final Scanner scanner = new Scanner(System.in);
 
     public int inputBonusNumber() {
+        System.out.println("\n보너스 번호를 입력해 주세요.");
         return scanner.nextInt();
     }
 }
