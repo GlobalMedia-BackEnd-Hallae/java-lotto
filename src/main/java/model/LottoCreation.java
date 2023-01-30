71a6c3807bc9e53a8d74988098818aed6364a308
Chan531 <chan657312@gmail.com>
2023-01-30 17:12:52 +0900

    feat : 구매한 개수만큼 로또 생성

---
+++ src/main/java/model/LottoCreation.java
@@ -0,0 +1,43 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class LottoCreation {
+
+    private static final int LOTTO_COUNT = 6;
+    private static final int LOTTO_NUMBER_RANGE = 45;
+
+    private final Random random = new Random();
+
+    public List<Lotto> createLotto(int number) {
+        final List<Lotto> createdLotto = new ArrayList<>();
+
+        while (createdLotto.size() != number) {
+            createdLotto.add(checkLotto());
+        }
+
+        return createdLotto;
+    }
+
+    private Lotto checkLotto() {
+        try {
+            return new Lotto(createRandomNumbers());
+        } catch (IllegalArgumentException e) {
+            return checkLotto();
+        }
+    }
+
+    private List<Integer> createRandomNumbers() {
+        final List<Integer> randomNumbers = new ArrayList<>();
+
+        for (int index = 0; index < LOTTO_COUNT; index++) {
+            randomNumbers.add(random.nextInt(LOTTO_NUMBER_RANGE) + 1);
+        }
+
+        Collections.sort(randomNumbers);
+        return randomNumbers;
+    }
+}
