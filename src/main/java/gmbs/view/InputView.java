0a0d4262e9372f9c6b184ae6fc3018e51ccd447f
JunSeop Kim <wnqtjq8958@naver.com>
2023-01-31 21:36:24 +0900

    fix : 구분자 수정

--- src/main/java/gmbs/view/InputView.java
+++ src/main/java/gmbs/view/InputView.java
@@ -1,14 +1,20 @@
 package gmbs.view;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
 
 public class InputView {
     private static final String INPUT_LOTTO_AMOUNT = "구입금액을 입력해 주세요.";
-    private static final String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_NUMBER = "보너스 볼을 입력해 주세요.";
     private static final String TYPE_ERROR = "[ERROR] 숫자만 입력 가능합니다.";
     private static final String BLANK = " ";
     private static final String EMPTY_STRING = "";
+    private static final String DELIMITER = ",";
 
     private static final Scanner SCANNER = new Scanner(System.in);
 
@@ -20,15 +26,24 @@ public class InputView {
         return convertToInt(SCANNER.nextLine());
     }
 
-    public static String inputWinningNumbers() {
-        System.out.println(INPUT_WINNING_NUMBERS);
-        return reduceBlank(SCANNER.nextLine());
+    public static List<Integer> inputLottoNumbers() {
+        String[] splitInput = split(reduceBlank(SCANNER.nextLine()));
+        return convertToIntegerList(splitInput);
     }
 
     private static String reduceBlank(String input) {
-        return input.replaceAll(BLANK, EMPTY_STRING);
+        return input.replace(BLANK, EMPTY_STRING);
     }
 
+    private static String[] split(String input) {
+        return input.split(DELIMITER);
+    }
+
+    private static List<Integer> convertToIntegerList(String[] array) {
+        return Arrays.stream(array)
+                .map(InputView::convertToInt)
+                .collect(collectingAndThen(toList(), Collections::unmodifiableList));
+    }
 
     public static int inputBonusBall() {
         System.out.println(INPUT_BONUS_NUMBER);
