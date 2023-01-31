c2fa1444548e9656c3241af4624e0ff156d82e0f
JunSeop Kim <wnqtjq8958@naver.com>
2023-01-31 21:45:26 +0900

    test : 테스트 항목 추가

--- src/test/java/gmbs/domain/LottoNumbersTest.java
+++ src/test/java/gmbs/domain/LottoNumbersTest.java
@@ -3,6 +3,12 @@ package gmbs.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -11,29 +17,44 @@ class LottoNumbersTest {
     @Test
     @DisplayName("중복된 숫자로 생성시 예외 발생")
     void incorrect() {
-        assertThatThrownBy(() -> new LottoNumbers("2,2,3,4,5,6")).isInstanceOf(IllegalArgumentException.class)
+        // given
+        List<Integer> numbers = getNumbers(2, 2, 3, 4, 5, 6);
+
+        // then
+        assertThatThrownBy(() -> new LottoNumbers(numbers)).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("로또 개수는 중복이 불가능합니다.");
     }
 
     @Test
     @DisplayName("숫자가 6개 미만")
     void incorrect4() {
-        assertThatThrownBy(() -> new LottoNumbers("2,5,6")).isInstanceOf(IllegalArgumentException.class)
+        // given
+        List<Integer> numbers = getNumbers(1, 2, 3, 4, 5);
+
+        // then
+        assertThatThrownBy(() -> new LottoNumbers(numbers)).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("로또 개수는 6개로 제한됩니다.");
     }
 
-    @Test
-    @DisplayName("문자열이 포함된 경우")
-    void incorrect2() {
-        assertThatThrownBy(() -> new LottoNumbers("1,aa,3,4,5,6")).isInstanceOf(IllegalArgumentException.class);
-    }
 
     @Test
     @DisplayName("equals 확인")
     void test() {
-        LottoNumbers lottoNumbers1 = new LottoNumbers("1,2,3,4,5,6");
-        LottoNumbers lottoNumbers2 = new LottoNumbers("5,2,1,3,4,6");
+        // given
+        List<Integer> numbers1 = getNumbers(1, 2, 3, 4, 5, 6);
+        List<Integer> numbers2 = getNumbers(5, 2, 1, 3, 4, 6);
 
+        LottoNumbers lottoNumbers1 = new LottoNumbers(numbers1);
+        LottoNumbers lottoNumbers2 = new LottoNumbers(numbers2);
+
+        // then
         assertThat(lottoNumbers1).isEqualTo(lottoNumbers2);
+    }
+
+
+    private List<Integer> getNumbers(int... numbers) {
+        return Arrays.stream(numbers)
+                .boxed()
+                .collect(collectingAndThen(toList(), Collections::unmodifiableList));
     }
 }
