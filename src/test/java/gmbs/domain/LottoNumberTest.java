c4f3b19d62f51558db8fc7906b4fa6739b0f8f95
JunSeop Kim <wnqtjq8958@naver.com>
2023-01-31 21:45:51 +0900

    test : hasMessage 추가

--- src/test/java/gmbs/domain/LottoNumberTest.java
+++ src/test/java/gmbs/domain/LottoNumberTest.java
@@ -9,11 +9,18 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoNumberTest {
 
+    private static final int MIN = 1;
+    private static final int MAX = 45;
+
+    private static final String NUMBER_RANGE_ERROR = String.format("[ERROR] 로또 숫자는 %d 이상 %d 이하의 숫자만 가능합니다", MIN, MAX);
+
     @ParameterizedTest
     @ValueSource(ints = {0, 46, -1})
     @DisplayName("숫자가 범위 내에 존재하지 않는 경우 예외 발생")
     void incorrect(int number) {
-        assertThatThrownBy(() -> LottoNumber.of(number)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> LottoNumber.of(number))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(NUMBER_RANGE_ERROR);
     }
 
     @ParameterizedTest
