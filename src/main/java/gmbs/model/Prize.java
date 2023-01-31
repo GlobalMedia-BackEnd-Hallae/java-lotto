6b70e0b702c9facb67957ef74e52ee789a75a17b
Sy P <yongsa0221@gmail.com>
2023-01-31 18:37:21 +0900

    feat : prize 확인 기능 추가

--- src/main/java/gmbs/model/Prize.java
+++ src/main/java/gmbs/model/Prize.java
@@ -1,21 +1,25 @@
 package gmbs.model;
 
+import java.util.Arrays;
+
 public enum Prize {
-    FIRST(2000000000, 1, 6),
-    SECOND(30000000, 2, 5),
-    THIRD(1500000, 3, 5),
-    FOURTH(50000, 4, 4),
-    FIFTH(5000, 5, 3),
-    LOSER(0, 6, 2);
+    FIRST(2000000000, 1, 6, false),
+    SECOND(30000000, 2, 5, true),
+    THIRD(1500000, 3, 5, false),
+    FOURTH(50000, 4, 4, false),
+    FIFTH(5000, 5, 3, false),
+    LOSER(0, 6, 2, false);
 
     private final int money;
     private final int place;
     private final int matches;
+    private final boolean requireBonus;
 
-    Prize(int money, int place, int matches) {
+    Prize(int money, int place, int matches, boolean requireBonus) {
         this.money = money;
         this.place = place;
         this.matches = matches;
+        this.requireBonus = requireBonus;
     }
 
     public int money() {
@@ -28,5 +32,16 @@ public enum Prize {
 
     public int matches() {
         return matches;
+    }
+
+    public boolean requireBonus() {
+        return requireBonus;
+    }
+
+    public static Prize find(int matches, boolean hasBonus) {
+        return Arrays.stream(values())
+                .filter(prize -> prize.matches == matches && prize.requireBonus == (prize.requireBonus() && hasBonus))
+                .findAny()
+                .orElse(LOSER);
     }
 }
