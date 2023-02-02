c35bab39cfb85fa62c9a3e7d878fdba79a452e1f
chanwoo <chocw0402@gmail.com>
2023-02-02 19:01:33 +0900

    feat : LottoNumber 관련 규칙 추상클래스

---
+++ src/main/java/gmbs/model/lotto/number/LottoNumberStrategy.java
@@ -0,0 +1,10 @@
+package gmbs.model.lotto.number;
+
+public abstract class LottoNumberStrategy {
+
+    protected static final int MIN_LOTTO_NUMBER = 1;
+    protected static final int MAX_LOTTO_NUMBER = 45;
+
+    protected LottoNumberStrategy() {
+    }
+}
