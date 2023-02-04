9ebc89632deef55dc921384c8f7bccabfdae5ed4
JunSeop Kim <wnqtjq8958@naver.com>
2023-02-04 15:59:53 +0900

    refactor : 테스트 이름 수정

--- src/test/java/gmbs/domain/LottoAmountTest.java
+++ src/test/java/gmbs/domain/LottoAmountTest.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class LottoAmountTest {
     @ParameterizedTest
     @ValueSource(ints = {0, 1100, -1})
-    @DisplayName("구매 단위 테스트, 최소 1000 이상, 1000단위로 구매")
+    @DisplayName("구매 단위 테스트, 최소 1000 이상, 1000단위로 구매하지 않을 시 예외 발생")
     void failed(int input) {
         assertThatThrownBy(() -> new LottoAmount(input))
                 .isInstanceOf(IllegalArgumentException.class);
