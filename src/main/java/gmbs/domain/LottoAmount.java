192dd73c57551651d46e143a4e60c2f11107df5f
JunSeop Kim <wnqtjq8958@naver.com>
2023-02-04 15:59:20 +0900

    fix : 로또 금액 상수 추가

--- src/main/java/gmbs/domain/LottoAmount.java
+++ src/main/java/gmbs/domain/LottoAmount.java
@@ -2,9 +2,9 @@ package gmbs.domain;
 
 public class LottoAmount {
     private static final int MIN_COST = 1000;
+    private static final int LOTTO_COST = 1000;
     private static final double DECIMAL = 1.0;
 
-    private static final String NOT_NUMBER_ERROR = "[ERROR] 금액은 숫자만 등록 가능합니다.";
     private static final String NOT_NATURAL_NUMBER_ERROR = "[ERROR] 금액은 " + MIN_COST + " 이상이어야 합니다.";
     private static final String NOT_DIVISIBLE_NUMBER_ERROR = "[ERROR] 금액은 " + MIN_COST + "단위여야 합니다.";
 
@@ -16,19 +16,11 @@ public class LottoAmount {
     }
 
     public int calculateLottoCount() {
-        return amount / MIN_COST;
+        return amount / LOTTO_COST;
     }
 
     public double calculateProfit(long prizeSum) {
         return DECIMAL * prizeSum / amount;
-    }
-
-    private int convertToInt(String input) {
-        try {
-            return Integer.parseInt(input);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(NOT_NUMBER_ERROR);
-        }
     }
 
     private void validateAmount(int amount) {
