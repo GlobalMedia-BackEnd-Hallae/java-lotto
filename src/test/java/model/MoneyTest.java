9dc05b7fbae8194b9dcd2fe2813d8d243a9510c2
Chan531 <chan657312@gmail.com>
2023-02-07 02:00:55 +0900

    test : Money Vo 테스트 수정

--- src/test/java/model/MoneyTest.java
+++ src/test/java/model/MoneyTest.java
@@ -2,6 +2,8 @@ package model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
@@ -9,7 +11,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 class MoneyTest {
 
     @Test
-    @DisplayName("조건을 충족하는 금액 전달받을 때 오류 없이 돈을 생성할 수 있다.")
+    @DisplayName("1000원 이상이며 1000으로 나누어 떨어지는 금액 전달받을 때 오류 없이 돈을 생성할 수 있다.")
     void canCreateMoney() {
         // given
         final String rightInput = "1000";
@@ -21,36 +23,12 @@ class MoneyTest {
         assertThat(money.getMoney()).isEqualTo(Integer.parseInt(rightInput));
     }
 
-    @Test
-    @DisplayName("숫자가 아닌 입력이 전달될 때 오류를 발생시킬 수 있다.")
-    void canCheckDigit() {
-        // given
-        final String notDigitInput = "abc123";
-
+    @ParameterizedTest
+    @DisplayName("숫자가 아니거나 1000보다 작거나 1000으로 나누어 떨어지지 않는 금액이 입력되어 Money 객체로 전달될 때 예외를 발생시킬 수 있다.")
+    @CsvSource({"abc", "0", "1001"})
+    void canCheckRest(String input) {
         // when, then
-        assertThatThrownBy(() -> new Money(notDigitInput))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    @DisplayName("1000원 보다 더 작은 금액이 입력될 때 오류를 발생시킬 수 있다.")
-    void canCheckRange() {
-        // given
-        final String smallerInput = "0";
-
-        // when, then
-        assertThatThrownBy(() -> new Money(smallerInput))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    @DisplayName("1000으로 나누어 떨어지지 않는 금액이 입력될 때 오류를 발생시킬 수 있다.")
-    void canCheckRest() {
-        // given
-        final String restInput = "1001";
-
-        // when, then
-        assertThatThrownBy(() -> new Money(restInput))
+        assertThatThrownBy(() -> new Money(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
