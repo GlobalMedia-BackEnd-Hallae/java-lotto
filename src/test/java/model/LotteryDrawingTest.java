543aae86eb1d224e82096324b6fd3bc1b7c74108
Chan531 <chan657312@gmail.com>
2023-02-07 01:47:21 +0900

    test : 로또 추첨 테스트

--- src/test/java/model/LotteryDrawingTest.java
+++ src/test/java/model/LotteryDrawingTest.java
@@ -5,125 +5,119 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
+import java.util.HashMap;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class LotteryDrawingTest {
 
     private final LotteryDrawing lotteryDrawing = new LotteryDrawing();
+    private final LottoNumber one = new LottoNumber(1);
+    private final LottoNumber two = new LottoNumber(2);
+    private final LottoNumber three = new LottoNumber(3);
+    private final LottoNumber four = new LottoNumber(4);
+    private final LottoNumber five = new LottoNumber(5);
+    private final LottoNumber six = new LottoNumber(6);
+    private final LottoNumber seven = new LottoNumber(7);
+    private final LottoNumber eight = new LottoNumber(8);
+    private final LottoNumber nine = new LottoNumber(9);
+    private final LottoNumber ten = new LottoNumber(10);
+    private final LottoNumber bonusNumber = new LottoNumber(7);
+    private final Lotto winningNumbers = new Lotto(Arrays.asList(one, two, three, four, five, six));
 
     @Test
     @DisplayName("1등을 추첨할 수 있다.")
     void canCheckFirstPlace() {
         // given
-        final List<Lotto> lotto = Collections.singletonList(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)));
-        final Lotto winningNumbers = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
-        winningNumbers.addBonusNumber(7);
-        final List<Integer> winningResult = Arrays.asList(0, 0, 0, 0, 1, 0);
+        final Lotto firstLotto = new Lotto(Arrays.asList(one, two, three, four, five, six));
+        final Lottery lottery = new Lottery(Collections.singletonList(firstLotto));
+        final HashMap<Winning, Integer> expectedWinningResult = new HashMap<>();
+        expectedWinningResult.put(Winning.FIRST, 1);
 
         // when
-        List<Integer> firstPlaceResult = lotteryDrawing.drawLottery(winningNumbers, lotto);
+        final HashMap<Winning, Integer> winningResult = lotteryDrawing.drawLottery(winningNumbers, bonusNumber, lottery);
 
         // then
-        assertThat(firstPlaceResult).isEqualTo(winningResult);
+        assertThat(winningResult).isEqualTo(expectedWinningResult);
     }
 
     @Test
     @DisplayName("2등을 추첨할 수 있다.")
     void canCheckSecondPlace() {
         // given
-        final List<Lotto> lotto = Collections.singletonList(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 7)));
-        final Lotto winningNumbers = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
-        winningNumbers.addBonusNumber(7);
-        final List<Integer> winningResult = Arrays.asList(0, 0, 0, 1, 0, 0);
+        final Lotto secondLotto = new Lotto(Arrays.asList(one, two, three, four, five, seven));
+        final Lottery lottery = new Lottery(Collections.singletonList(secondLotto));
+        final HashMap<Winning, Integer> expectedWinningResult = new HashMap<>();
+        expectedWinningResult.put(Winning.SECOND, 1);
 
         // when
-        List<Integer> secondPlaceResult = lotteryDrawing.drawLottery(winningNumbers, lotto);
+        final HashMap<Winning, Integer> winningResult = lotteryDrawing.drawLottery(winningNumbers, bonusNumber, lottery);
 
         // then
-        assertThat(secondPlaceResult).isEqualTo(winningResult);
+        assertThat(winningResult).isEqualTo(expectedWinningResult);
     }
 
     @Test
     @DisplayName("3등을 추첨할 수 있다.")
     void canCheckThirdPlace() {
         // given
-        final List<Lotto> lotto = Collections.singletonList(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 7)));
-        final Lotto winningNumbers = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
-        winningNumbers.addBonusNumber(8);
-        final List<Integer> winningResult = Arrays.asList(0, 0, 1, 0, 0, 0);
+        final Lotto thirdLotto = new Lotto(Arrays.asList(one, two, three, four, five, eight));
+        final Lottery lottery = new Lottery(Collections.singletonList(thirdLotto));
+        final HashMap<Winning, Integer> expectedWinningResult = new HashMap<>();
+        expectedWinningResult.put(Winning.THIRD, 1);
 
         // when
-        List<Integer> thirdPlaceResult = lotteryDrawing.drawLottery(winningNumbers, lotto);
+        final HashMap<Winning, Integer> winningResult = lotteryDrawing.drawLottery(winningNumbers, bonusNumber, lottery);
 
         // then
-        assertThat(thirdPlaceResult).isEqualTo(winningResult);
+        assertThat(winningResult).isEqualTo(expectedWinningResult);
     }
 
     @Test
     @DisplayName("4등을 추첨할 수 있다.")
     void canCheckFourthPlace() {
         // given
-        final List<Lotto> lotto = Collections.singletonList(new Lotto(Arrays.asList(1, 2, 3, 4, 7, 8)));
-        final Lotto winningNumbers = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
-        winningNumbers.addBonusNumber(9);
-        final List<Integer> winningResult = Arrays.asList(0, 1, 0, 0, 0, 0);
+        final Lotto fourthLotto = new Lotto(Arrays.asList(one, two, three, four, seven, eight));
+        final Lottery lottery = new Lottery(Collections.singletonList(fourthLotto));
+        final HashMap<Winning, Integer> expectedWinningResult = new HashMap<>();
+        expectedWinningResult.put(Winning.FOURTH, 1);
 
         // when
-        List<Integer> fourthPlaceResult = lotteryDrawing.drawLottery(winningNumbers, lotto);
+        final HashMap<Winning, Integer> winningResult = lotteryDrawing.drawLottery(winningNumbers, bonusNumber, lottery);
 
         // then
-        assertThat(fourthPlaceResult).isEqualTo(winningResult);
+        assertThat(winningResult).isEqualTo(expectedWinningResult);
     }
 
     @Test
     @DisplayName("5등을 추첨할 수 있다.")
     void canCheckFifthPlace() {
         // given
-        final List<Lotto> lotto = Collections.singletonList(new Lotto(Arrays.asList(1, 2, 3, 7, 8, 9)));
-        final Lotto winningNumbers = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
-        winningNumbers.addBonusNumber(10);
-        final List<Integer> winningResult = Arrays.asList(1, 0, 0, 0, 0, 0);
+        final Lotto fifthLotto = new Lotto(Arrays.asList(one, two, three, seven, eight, nine));
+        final Lottery lottery = new Lottery(Collections.singletonList(fifthLotto));
+        final HashMap<Winning, Integer> expectedWinningResult = new HashMap<>();
+        expectedWinningResult.put(Winning.FIFTH, 1);
 
         // when
-        List<Integer> fifthPlaceResult = lotteryDrawing.drawLottery(winningNumbers, lotto);
+        final HashMap<Winning, Integer> winningResult = lotteryDrawing.drawLottery(winningNumbers, bonusNumber, lottery);
 
         // then
-        assertThat(fifthPlaceResult).isEqualTo(winningResult);
+        assertThat(winningResult).isEqualTo(expectedWinningResult);
     }
 
     @Test
     @DisplayName("꽝을 추첨할 수 있다.")
     void canCheckNotWin() {
         // given
-        final List<Lotto> lotto = Collections.singletonList(new Lotto(Arrays.asList(1, 2, 7, 8, 9, 10)));
-        final Lotto winningNumbers = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
-        winningNumbers.addBonusNumber(11);
-        final List<Integer> winningResult = Arrays.asList(0, 0, 0, 0, 0, 1);
+        final Lotto failLotto = new Lotto(Arrays.asList(one, two, seven, eight, nine, ten));
+        final Lottery lottery = new Lottery(Collections.singletonList(failLotto));
+        final HashMap<Winning, Integer> expectedWinningResult = new HashMap<>();
+        expectedWinningResult.put(Winning.FAIL, 1);
 
         // when
-        List<Integer> notWinResult = lotteryDrawing.drawLottery(winningNumbers, lotto);
+        final HashMap<Winning, Integer> winningResult = lotteryDrawing.drawLottery(winningNumbers, bonusNumber, lottery);
 
         // then
-        assertThat(notWinResult).isEqualTo(winningResult);
-    }
-
-    @Test
-    @DisplayName("여러 개의 당첨을 추첨할 수 있다.")
-    void canCheckSeveralWins() {
-        // given
-        final List<Integer> firstLotto = Arrays.asList(1, 2, 3, 4, 5, 6);
-        final List<Integer> secondLotto = Arrays.asList(7, 8, 9, 10, 11, 12);
-        final List<Lotto> lotto = Arrays.asList(new Lotto(firstLotto), new Lotto(secondLotto));
-        final Lotto winningNumbers = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
-        winningNumbers.addBonusNumber(7);
-        final List<Integer> winningResult = Arrays.asList(0, 0, 0, 0, 1, 1);
-
-        // when
-        List<Integer> severalResults = lotteryDrawing.drawLottery(winningNumbers, lotto);
-
-        // then
-        assertThat(severalResults).isEqualTo(winningResult);
+        assertThat(winningResult).isEqualTo(expectedWinningResult);
     }
 }
