de12d725bced5a3a76d8ecf51864524174a71055
Chan531 <chan657312@gmail.com>
2023-02-07 01:46:07 +0900

    feat : 로또 게임 입력 모음

--- src/main/java/view/Input.java
+++ src/main/java/view/Input.java
@@ -1,5 +1,9 @@
 package view;
 
+import model.LottoNumber;
+import model.Money;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -8,24 +12,39 @@ import java.util.stream.Collectors;
 public class Input {
 
     private final static String COMMA = ",";
+    private final static String ENTER = "\n";
 
     private final Scanner scanner = new Scanner(System.in);
 
-    public String inputMoney() {
+    public Money inputMoney() {
         System.out.println("구입금액을 입력해 주세요.");
-        return scanner.nextLine();
+        return new Money(scanner.nextLine());
     }
 
-    public List<Integer> inputWinningNumber() {
+    public List<LottoNumber> inputWinningNumber() {
         System.out.println("당첨 번호를 입력해 주세요.");
+        return convertNumbersToLotto(convertInput());
+    }
+
+    private List<Integer> convertInput() {
         return Arrays.stream(Arrays.stream(scanner.nextLine().split(COMMA))
                         .mapToInt(Integer::parseInt)
                         .toArray()).boxed()
-                .collect(Collectors.toList());
+                .collect(Collectors.toUnmodifiableList());
     }
 
-    public int inputBonusNumber() {
-        System.out.println("\n보너스 번호를 입력해 주세요.");
-        return scanner.nextInt();
+    private List<LottoNumber> convertNumbersToLotto(List<Integer> randomNumbers) {
+        final List<LottoNumber> lotto = new ArrayList<>();
+
+        for (int randomNumber : randomNumbers) {
+            lotto.add(new LottoNumber(randomNumber));
+        }
+
+        return lotto;
+    }
+
+    public LottoNumber inputBonusNumber() {
+        System.out.println(ENTER + "보너스 번호를 입력해 주세요.");
+        return new LottoNumber(scanner.nextInt());
     }
 }
