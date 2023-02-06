70ba7b48615f102eccb7b7cf6952d7d7ecd12de1
Chan531 <chan657312@gmail.com>
2023-02-07 01:46:28 +0900

    feat : 돈 VO

--- src/main/java/model/Money.java
+++ src/main/java/model/Money.java
@@ -1,21 +1,22 @@
 package model;
 
+import java.util.Objects;
+
 public class Money {
 
     private static final String REGEX = "[0-9]+";
     private static final int MIN_MONEY_VALUE = 1000;
     private final int money;
 
-    // 공백 제외 8줄
     public Money(String input) {
         if (checkDigit(input)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해주세요.");
         }
 
         final int money = Integer.parseInt(input);
 
         if (checkRange(money) || checkRest(money)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 1000원 이상이며 1000으로 나누어 떨어지는 금액을 입력해주세요.");
         }
 
         this.money = money;
@@ -35,5 +36,18 @@ public class Money {
 
     private boolean checkRest(int money) {
         return money % MIN_MONEY_VALUE != 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money1 = (Money) o;
+        return money == money1.money;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(money);
     }
 }
