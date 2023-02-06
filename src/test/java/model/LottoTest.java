10bd87bd76cedc56cf7b24ca1fcaf44c0bcda751
Chan531 <chan657312@gmail.com>
2023-02-07 01:40:20 +0900

    test : 로또 test

--- src/test/java/model/LottoTest.java
+++ src/test/java/model/LottoTest.java
@@ -11,96 +11,107 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class LottoTest {
 
+    private final LottoNumber one = new LottoNumber(1);
+    private final LottoNumber two = new LottoNumber(2);
+    private final LottoNumber three = new LottoNumber(3);
+    private final LottoNumber four = new LottoNumber(4);
+    private final LottoNumber five = new LottoNumber(5);
+    private final LottoNumber six = new LottoNumber(6);
+    private final LottoNumber seven = new LottoNumber(7);
+    private final List<LottoNumber> LottoNumbers = Arrays.asList(one, two, three, four, five, six);
+
     @Test
-    @DisplayName("조건을 충족하는 번호를 전달받을 때 오류 없이 로또를 생성할 수 있다.")
+    @DisplayName("중복이 없으며 범위가 1 이상 45 이하인 6개의 로또 번호 리스트를 전달받을 때 로또 객체를 생성할 수 있다.")
     void canCreateLotto() {
-        // given
-        final List<Integer> rightInput = Arrays.asList(1, 2, 3, 4, 5, 6);
-
-        // when
-        final Lotto lotto = new Lotto(rightInput);
+        // given, when
+        final Lotto lotto = new Lotto(LottoNumbers);
 
         // then
-        assertThat(lotto.getLotto()).isEqualTo(rightInput);
+        assertThat(lotto.getLotto()).isEqualTo(LottoNumbers);
     }
 
     @Test
-    @DisplayName("조건을 충족하는 보너스 번호를 전달받을 때 이미 생성된 당첨번호 로또에 보너스 번호를 오류 없이 전달할 수 있다.")
-    void canSendBonusNumberAtCreatedLotto() {
+    @DisplayName("중복이 있는 로또 번호 리스트를 전달받을 때 예외를 발생시킬 수 있다.")
+    void canThrowExceptionWhenLottoOverlap() {
         // given
-        final Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
-        final int bonusNumber = 7;
+        final List<LottoNumber> input = Arrays.asList(one, two, three, four, five, five);
+
+        // when, then
+        assertThatThrownBy(() -> new Lotto(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("크기가 6이 아닌 로또 번호 리스트를 전달 받을 때 예외를 발생시킬 수 있다.")
+    void canThrowExceptionWhenWierdLotto() {
+        // given
+        final List<LottoNumber> largeLotto = Arrays.asList(one, two, three, four, five, six, seven);
+        final List<LottoNumber> smallLotto = Arrays.asList(one, two, three, four, five);
+
+        // when, then
+        assertThatThrownBy(() -> new Lotto(largeLotto))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Lotto(smallLotto))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("로또 번호와 중복되는 보너스 번호를 전달받을 때 예외를 발생시킬 수 있다.")
+    void canThrowExceptionWhenBonusNumberOverlap() {
+        // given
+        final Lotto lotto = new Lotto(LottoNumbers);
+        final LottoNumber bonusNumber = one;
+
+        // when, then
+        assertThatThrownBy(() -> lotto.checkBonusNumberOverlap(bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("입력된 당첨 번호와 비교하여 당첨된 번호의 개수를 알 수 있다.")
+    void canDrawLottoWithWinningNumbers() {
+        // given
+        final Lotto lotto = new Lotto(LottoNumbers);
+        final Lotto winningNumbers = new Lotto(LottoNumbers);
 
         // when
-        lotto.addBonusNumber(bonusNumber);
+        final int count = lotto.drawLottoWithWinningNumbers(winningNumbers);
 
         // then
-        assertThat(lotto.getBonusNumber()).isEqualTo(bonusNumber);
+        assertThat(count).isEqualTo(6);
     }
 
     @Test
-    @DisplayName("중복된 번호를 전달받을 때 오류를 발생시킬 수 있다.")
-    void canCheckOverlap() {
+    @DisplayName("입력된 로또 번호와 비교하여 당첨 번호 중에 같은 번호가 있다면 1을, 없다면 0을 반환할 수 있다.")
+    void canCompareLottoNumberWithWinningNumber() {
         // given
-        final List<Integer> overlapInput = Arrays.asList(1, 2, 3, 4, 5, 5);
+        final Lotto winningNumbers = new Lotto(LottoNumbers);
+        final LottoNumber sameNumber = one;
+        final LottoNumber differentNumber = seven;
 
-        // when, then
-        assertThatThrownBy(() -> new Lotto(overlapInput))
-                .isInstanceOf(IllegalArgumentException.class);
+        // when
+        final int same = winningNumbers.compareLottoNumberWithWinningNumber(sameNumber);
+        final int different = winningNumbers.compareLottoNumberWithWinningNumber(differentNumber);
+
+        // then
+        assertThat(same).isEqualTo(1);
+        assertThat(different).isEqualTo(0);
     }
 
     @Test
-    @DisplayName("범위를 벗어나는 숫자를 전달받을 때 오류를 발생시킬 수 있다.")
-    void canCheckRange() {
+    @DisplayName("입력된 보너스 번호와 비교하여 같은 번호가 있다면 1을 아니라면 0을 반환할 수 있다.")
+    void canDrawLottoWithBonusNumber() {
         // given
-        final List<Integer> smallerInput = Arrays.asList(0, 2, 3, 4, 5, 6);
-        final List<Integer> biggerInput = Arrays.asList(1, 2, 3, 4, 5, 46);
+        final LottoNumber sameBonusNumber = one;
+        final LottoNumber differentBonusNumber = seven;
+        final Lotto lotto = new Lotto(LottoNumbers);
 
-        // when, then
-        assertThatThrownBy(() -> new Lotto(smallerInput))
-                .isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> new Lotto(biggerInput))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+        // when
+        final int same = lotto.compareLottoNumberWithBonusNumber(sameBonusNumber);
+        final int different = lotto.compareLottoNumberWithBonusNumber(differentBonusNumber);
 
-    @Test
-    @DisplayName("전달받은 숫자가 6개가 아닐 때 오류를 발생시킬 수 있다.")
-    void canCheckCount() {
-        // given
-        final List<Integer> lowerInput = Arrays.asList(1, 2, 3, 4, 5);
-        final List<Integer> upperInput = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
-
-        // when, then
-        assertThatThrownBy(() -> new Lotto(lowerInput))
-                .isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> new Lotto(upperInput))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    @DisplayName("범위를 벗어나는 보너스 번호를 전달받을 때 오류를 발생시킬 수 있다.")
-    void canCheckBonusNumberRange() {
-        // given
-        final Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
-        final int smallerBonusNumber = 0;
-        final int biggerBonusNumber = 46;
-
-        // when, then
-        assertThatThrownBy(() -> lotto.addBonusNumber(smallerBonusNumber))
-                .isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> lotto.addBonusNumber(biggerBonusNumber))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    @DisplayName("중복된 보너스 번호를 전달받을 때 오류를 발생시킬 수 있다.")
-    void canCheckBonusNumberOverlap() {
-        // given
-        final Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
-        final int bonusNumber = 6;
-
-        // when, then
-        assertThatThrownBy(() -> lotto.addBonusNumber(bonusNumber))
-                .isInstanceOf(IllegalArgumentException.class);
+        // then
+        assertThat(same).isEqualTo(1);
+        assertThat(different).isEqualTo(0);
     }
 }
