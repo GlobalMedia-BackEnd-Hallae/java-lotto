eef8066a2a307e78aaa5a7335911cdc22b2dfb45
Chan531 <chan657312@gmail.com>
2023-02-13 18:24:46 +0900

    test : ParameterizedTest로 중복 테스트 삭제

--- src/test/java/model/LottoTest.java
+++ src/test/java/model/LottoTest.java
@@ -2,22 +2,26 @@ package model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class LottoTest {
 
-    private final LottoNumber one = new LottoNumber(1);
-    private final LottoNumber two = new LottoNumber(2);
-    private final LottoNumber three = new LottoNumber(3);
-    private final LottoNumber four = new LottoNumber(4);
-    private final LottoNumber five = new LottoNumber(5);
-    private final LottoNumber six = new LottoNumber(6);
-    private final LottoNumber seven = new LottoNumber(7);
+    private static final LottoNumber one = new LottoNumber(1);
+    private static final LottoNumber two = new LottoNumber(2);
+    private static final LottoNumber three = new LottoNumber(3);
+    private static final LottoNumber four = new LottoNumber(4);
+    private static final LottoNumber five = new LottoNumber(5);
+    private static final LottoNumber six = new LottoNumber(6);
+    private static final LottoNumber seven = new LottoNumber(7);
     private final List<LottoNumber> LottoNumbers = Arrays.asList(one, two, three, four, five, six);
 
     @Test
@@ -41,18 +45,20 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("크기가 6이 아닌 로또 번호 리스트를 전달 받을 때 예외를 발생시킬 수 있다.")
-    void canThrowExceptionWhenWierdLotto() {
-        // given
-        final List<LottoNumber> largeLotto = Arrays.asList(one, two, three, four, five, six, seven);
-        final List<LottoNumber> smallLotto = Arrays.asList(one, two, three, four, five);
-
+    @MethodSource("provideLargeLottoAndSmallLotto")
+    void canThrowExceptionWhenWierdLotto(List<LottoNumber> lotto) {
         // when, then
-        assertThatThrownBy(() -> new Lotto(largeLotto))
+        assertThatThrownBy(() -> new Lotto(lotto))
                 .isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> new Lotto(smallLotto))
-                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private static Stream<Arguments> provideLargeLottoAndSmallLotto() {
+        return Stream.of(
+                Arguments.of(List.of(one, two, three, four, five, six, seven)),
+                Arguments.of(List.of(one, two, three, four, five))
+        );
     }
 
     @Test
@@ -81,37 +87,45 @@ class LottoTest {
         assertThat(count).isEqualTo(6);
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("입력된 로또 번호와 비교하여 당첨 번호 중에 같은 번호가 있다면 1을, 없다면 0을 반환할 수 있다.")
-    void canCompareLottoNumberWithWinningNumber() {
+    @MethodSource("provideSameLottoNumberAndDifferentLottoNumber")
+    void canCompareLottoNumberWithWinningNumber(LottoNumber lottoNumber, int expectedResult) {
         // given
         final Lotto winningNumbers = new Lotto(LottoNumbers);
-        final LottoNumber sameNumber = one;
-        final LottoNumber differentNumber = seven;
 
         // when
-        final int same = winningNumbers.compareLottoNumberWithWinningNumber(sameNumber);
-        final int different = winningNumbers.compareLottoNumberWithWinningNumber(differentNumber);
+        final int result = winningNumbers.compareLottoNumberWithWinningNumber(lottoNumber);
 
         // then
-        assertThat(same).isEqualTo(1);
-        assertThat(different).isEqualTo(0);
+        assertThat(result).isEqualTo(expectedResult);
     }
 
-    @Test
+    private static Stream<Arguments> provideSameLottoNumberAndDifferentLottoNumber() {
+        return Stream.of(
+                Arguments.of(one, 1),
+                Arguments.of(seven, 0)
+        );
+    }
+
+    @ParameterizedTest
     @DisplayName("입력된 보너스 번호와 비교하여 같은 번호가 있다면 1을 아니라면 0을 반환할 수 있다.")
-    void canDrawLottoWithBonusNumber() {
+    @MethodSource("provideSameBonusNumberAndDifferentBonusNumber")
+    void canDrawLottoWithBonusNumber(LottoNumber bonusNumber, int expectedResult) {
         // given
-        final LottoNumber sameBonusNumber = one;
-        final LottoNumber differentBonusNumber = seven;
         final Lotto lotto = new Lotto(LottoNumbers);
 
         // when
-        final int same = lotto.compareLottoNumberWithBonusNumber(sameBonusNumber);
-        final int different = lotto.compareLottoNumberWithBonusNumber(differentBonusNumber);
+        final int result = lotto.compareLottoNumberWithBonusNumber(bonusNumber);
 
         // then
-        assertThat(same).isEqualTo(1);
-        assertThat(different).isEqualTo(0);
+        assertThat(result).isEqualTo(expectedResult);
+    }
+
+    private static Stream<Arguments> provideSameBonusNumberAndDifferentBonusNumber() {
+        return Stream.of(
+                Arguments.of(one, 1),
+                Arguments.of(seven, 0)
+        );
     }
 }
