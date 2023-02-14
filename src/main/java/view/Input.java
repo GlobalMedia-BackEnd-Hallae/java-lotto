000a972e7b12a5ecab8b1936d15ec38b3adc3c39
Chan531 <chan657312@gmail.com>
2023-02-14 16:34:10 +0900

    refactor : 메서드 가독성 개선

--- src/main/java/view/Input.java
+++ src/main/java/view/Input.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Input {
 
@@ -23,14 +24,12 @@ public class Input {
 
     public List<LottoNumber> inputWinningNumber() {
         System.out.println("당첨 번호를 입력해 주세요.");
-        return convertNumbersToLotto(convertInput());
+        return convertNumbersToLotto(convertInput(scanner.nextLine()));
     }
 
-    private List<Integer> convertInput() {
-        return Arrays.stream(Arrays.stream(scanner.nextLine().split(COMMA))
-                        .mapToInt(Integer::parseInt)
-                        .toArray()).boxed()
-                .collect(Collectors.toUnmodifiableList());
+    private List<Integer> convertInput(String input) {
+        Stream<Integer> lotto = Arrays.stream(Arrays.stream(input.split(COMMA)).mapToInt(Integer::parseInt).toArray()).boxed();
+        return lotto.collect(Collectors.toUnmodifiableList());
     }
 
     private List<LottoNumber> convertNumbersToLotto(List<Integer> randomNumbers) {
