aa109b8d619c2bf92a7b203e3f2758dfb728084f
Chan531 <chan657312@gmail.com>
2023-02-15 21:04:44 +0900

    refactor : 당첨된 로또 숫자 개수 판별 테스트 변경

--- src/test/java/model/LottoTest.java
+++ src/test/java/model/LottoTest.java
@@ -21,7 +21,19 @@ class LottoTest {
     private static final LottoNumber five = new LottoNumber(5);
     private static final LottoNumber six = new LottoNumber(6);
     private static final LottoNumber seven = new LottoNumber(7);
-    private final List<LottoNumber> LottoNumbers = Arrays.asList(one, two, three, four, five, six);
+    private static final LottoNumber eight = new LottoNumber(8);
+    private static final LottoNumber nine = new LottoNumber(9);
+    private static final LottoNumber ten = new LottoNumber(10);
+    private static final LottoNumber eleven = new LottoNumber(11);
+    private static final LottoNumber twelve = new LottoNumber(12);
+    private static final List<LottoNumber> LottoNumbers = List.of(one, two, three, four, five, six);
+    private static final Lotto allMatchLotto = new Lotto(List.of(one, two, three, four, five, six));
+    private static final Lotto fiveMatchLotto = new Lotto(List.of(one, two, three, four, five, seven));
+    private static final Lotto fourMatchLotto = new Lotto(List.of(one, two, three, four, seven, eight));
+    private static final Lotto threeMatchLotto = new Lotto(List.of(one, two, three, seven, eight, nine));
+    private static final Lotto twoMatchLotto = new Lotto(List.of(one, two, seven, eight, nine, ten));
+    private static final Lotto oneMatchLotto = new Lotto(List.of(one, seven, eight, nine, ten, eleven));
+    private static final Lotto zeroMatchLotto = new Lotto(List.of(seven, eight, nine, ten, eleven, twelve));
 
     @Test
     @DisplayName("중복이 없으며 범위가 1 이상 45 이하인 6개의 로또 번호 리스트를 전달받을 때 로또 객체를 생성할 수 있다.")
@@ -38,7 +50,8 @@ class LottoTest {
 
         // when, then
         assertThatThrownBy(() -> new Lotto(input))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 중복이 아닌 숫자를 입력해주세요.");
     }
 
     @ParameterizedTest
@@ -47,7 +60,8 @@ class LottoTest {
     void canThrowExceptionWhenWierdLotto(List<LottoNumber> lotto) {
         // when, then
         assertThatThrownBy(() -> new Lotto(lotto))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 6개의 숫자를 입력해주세요.");
     }
 
     private static Stream<Arguments> provideLargeLottoAndSmallLotto() {
@@ -57,38 +71,29 @@ class LottoTest {
         );
     }
 
-    @Test
-    @DisplayName("입력된 당첨 번호와 비교하여 당첨된 번호의 개수를 반환한다.")
-    void canDrawLottoWithWinningNumbers() {
-        // given
-        final Lotto lotto = new Lotto(LottoNumbers);
-        final Lotto winningNumbers = new Lotto(LottoNumbers);
-
-        // when
-        final int count = lotto.drawLottoWithWinningNumbers(winningNumbers);
-
-        // then
-        assertThat(count).isEqualTo(6);
-    }
-
     @ParameterizedTest
-    @DisplayName("입력된 로또 번호와 비교하여 당첨 번호 중에 같은 번호가 있다면 1을, 없다면 0을 반환한다.")
-    @MethodSource("provideSameLottoNumberAndDifferentLottoNumber")
-    void canCompareLottoNumberWithWinningNumber(LottoNumber lottoNumber, int expectedResult) {
+    @DisplayName("입력된 당첨 번호와 비교하여 당첨된 번호의 개수를 반환한다.")
+    @MethodSource("provideAllCasesLottos")
+    void canDrawLottoWithWinningNumbers(Lotto lottoNumbers, int expectedResult) {
         // given
         final Lotto winningNumbers = new Lotto(LottoNumbers);
 
         // when
-        final int result = winningNumbers.compareLottoNumberWithWinningNumber(lottoNumber);
+        final int result = lottoNumbers.drawLottoWithWinningNumbers(winningNumbers);
 
         // then
         assertThat(result).isEqualTo(expectedResult);
     }
 
-    private static Stream<Arguments> provideSameLottoNumberAndDifferentLottoNumber() {
+    private static Stream<Arguments> provideAllCasesLottos() {
         return Stream.of(
-                Arguments.of(one, 1),
-                Arguments.of(seven, 0)
+            Arguments.of(allMatchLotto, 6),
+            Arguments.of(fiveMatchLotto, 5),
+            Arguments.of(fourMatchLotto, 4),
+            Arguments.of(threeMatchLotto, 3),
+            Arguments.of(twoMatchLotto, 2),
+            Arguments.of(oneMatchLotto, 1),
+            Arguments.of(zeroMatchLotto, 0)
         );
     }
 
@@ -100,7 +105,7 @@ class LottoTest {
         final Lotto lotto = new Lotto(LottoNumbers);
 
         // when
-        final int result = lotto.compareLottoNumberWithBonusNumber(bonusNumber);
+        final int result = lotto.drawLottoWithBonusNumber(bonusNumber);
 
         // then
         assertThat(result).isEqualTo(expectedResult);
