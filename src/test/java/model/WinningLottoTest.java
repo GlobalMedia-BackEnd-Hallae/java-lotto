26e436fc63317d103146d0ee2c0730196deaf30d
Chan531 <chan657312@gmail.com>
2023-02-15 20:56:18 +0900

    refactor : hasMessage() 추가

--- src/test/java/model/WinningLottoTest.java
+++ src/test/java/model/WinningLottoTest.java
@@ -17,7 +17,7 @@ class WinningLottoTest {
     private final LottoNumber six = new LottoNumber(6);
 
     @Test
-    @DisplayName("당첨 로또 번호와 중복되는 보너스 번호를 전달받을 때 예외를 발생시킨다.")
+    @DisplayName("당첨 번호와 중복되는 보너스 번호를 전달받을 때 예외를 발생시킨다.")
     void canThrowExceptionWhenBonusNumberOverlap() {
         // given
         final WinningLotto winningLotto = new WinningLotto(new Lotto(List.of(one, two, three, four, five, six)));
@@ -25,6 +25,7 @@ class WinningLottoTest {
 
         // when, then
         assertThatThrownBy(() -> winningLotto.checkBonusNumberOverlap(bonusNumber))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 중복이 아닌 보너스 번호를 입력해주세요.");
     }
 }
