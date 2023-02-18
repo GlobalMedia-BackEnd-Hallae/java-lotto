0bafcd27d5fd679bc1bd9cf2f3dd13a5aad790a8
Chan531 <chan657312@gmail.com>
2023-02-18 20:17:11 +0900

    refactor : 메서드를 활용해 필드 정리

--- src/test/java/model/LotteryTest.java
+++ src/test/java/model/LotteryTest.java
@@ -5,37 +5,18 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class LotteryTest {
 
-    private final LottoNumber one = new LottoNumber(1);
-    private final LottoNumber two = new LottoNumber(2);
-    private final LottoNumber three = new LottoNumber(3);
-    private final LottoNumber four = new LottoNumber(4);
-    private final LottoNumber five = new LottoNumber(5);
-    private final LottoNumber six = new LottoNumber(6);
-    private final LottoNumber seven = new LottoNumber(7);
-    private final LottoNumber eight = new LottoNumber(8);
-    private final LottoNumber nine = new LottoNumber(9);
-    private final LottoNumber ten = new LottoNumber(10);
-    private final LottoNumber eleven = new LottoNumber(11);
-    private final LottoNumber twelve = new LottoNumber(12);
-    private final Lotto lotto1 = new Lotto(List.of(one, two, three, four, five, six));
-    private final Lotto lotto2 = new Lotto(List.of(one, two, three, four, five, seven));
-    private final Lotto lotto3 = new Lotto(List.of(one, two, three, four, seven, eight));
-    private final Lotto lotto4 = new Lotto(List.of(one, two, three, seven, eight, nine));
-    private final Lotto lotto5 = new Lotto(List.of(one, two, seven, eight, nine, ten));
-    private final Lotto lotto6 = new Lotto(List.of(one, seven, eight, nine, ten, eleven));
-    private final Lotto lotto7 = new Lotto(List.of(seven, eight, nine, ten, eleven, twelve));
-
     @Test
     @DisplayName("로또 묶음과 입력된 당첨 번호를 비교하여 같은 숫자의 개수를 담는 리스트를 반환한다.")
     void canDrawLottosWithWinningNumbers() {
         // given
-        final Lottery lottery = new Lottery(Arrays.asList(lotto1, lotto2, lotto3, lotto4, lotto5, lotto6, lotto7));
-        final Lotto winningNumbers = new Lotto(Arrays.asList(one, two, three, four, five, six));
+        final Lottery lottery = new Lottery(List.of(createLotto(1, 2, 3, 4, 5, 6), createLotto(1, 2, 3, 4, 5, 7), createLotto(1, 2, 3, 4, 7, 8), createLotto(1, 2, 3, 7, 8, 9), createLotto(1, 2, 7, 8, 9, 10), createLotto(1, 7, 8, 9, 10, 11), createLotto(7, 8, 9, 10, 11, 12)));
+        final Lotto winningNumbers = createLotto(1, 2, 3, 4, 5, 6);
         final List<Integer> expectedResult = Arrays.asList(6, 5, 4, 3, 2, 1, 0);
 
         // when
@@ -49,8 +30,8 @@ class LotteryTest {
     @DisplayName("로또 묶음과 입력된 보너스 번호를 비교하여 같은 번호가 있다면 1을, 없다면 0을 담는 리스트를 반환한다.")
     void canDrawLottosWithBonusNumber() {
         // given
-        final Lottery lottery = new Lottery(Arrays.asList(lotto1, lotto7));
-        final LottoNumber bonusNumber = one;
+        final Lottery lottery = new Lottery(List.of(createLotto(1, 2, 3, 4, 5, 6), createLotto(7, 8, 9, 10, 11, 12)));
+        final LottoNumber bonusNumber = new LottoNumber(1);
         final List<Boolean> expectedResult = Arrays.asList(true, false);
 
         // when
@@ -58,5 +39,11 @@ class LotteryTest {
 
         // then
         assertThat(result).isEqualTo(expectedResult);
+    }
+
+    private Lotto createLotto(int... numbers) {
+        return new Lotto(Arrays.stream(numbers)
+                .mapToObj(LottoNumber::new)
+                .collect(Collectors.toUnmodifiableList()));
     }
 }
