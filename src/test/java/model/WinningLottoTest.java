f21eb779d9e0d6c1246880c745989d5e9a915698
Chan531 <chan657312@gmail.com>
2023-02-18 20:18:27 +0900

    refactor : 메서드를 활용해 필드 정리

--- src/test/java/model/WinningLottoTest.java
+++ src/test/java/model/WinningLottoTest.java
@@ -3,29 +3,29 @@ package model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class WinningLottoTest {
 
-    private final LottoNumber one = new LottoNumber(1);
-    private final LottoNumber two = new LottoNumber(2);
-    private final LottoNumber three = new LottoNumber(3);
-    private final LottoNumber four = new LottoNumber(4);
-    private final LottoNumber five = new LottoNumber(5);
-    private final LottoNumber six = new LottoNumber(6);
-
     @Test
     @DisplayName("당첨 번호와 중복되는 보너스 번호를 전달받을 때 예외를 발생시킨다.")
     void canThrowExceptionWhenBonusNumberOverlap() {
         // given
-        final WinningLotto winningLotto = new WinningLotto(new Lotto(List.of(one, two, three, four, five, six)));
-        final LottoNumber bonusNumber = six;
+        final WinningLotto winningLotto = new WinningLotto(createLotto(1, 2, 3, 4, 5, 6));
+        final LottoNumber bonusNumber = new LottoNumber(6);
 
         // when, then
         assertThatThrownBy(() -> winningLotto.checkBonusNumberOverlap(bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 중복이 아닌 보너스 번호를 입력해주세요.");
+    }
+
+    private Lotto createLotto(int... numbers) {
+        return new Lotto(Arrays.stream(numbers)
+                .mapToObj(LottoNumber::new)
+                .collect(Collectors.toUnmodifiableList()));
     }
 }
