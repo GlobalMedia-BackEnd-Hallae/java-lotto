ed08dae87cbb2aabac538e63eaaa143bccf75dd7
Chan531 <chan657312@gmail.com>
2023-02-20 00:48:32 +0900

    refactor : 필드 정리

--- src/test/java/model/LotteryDrawingTest.java
+++ src/test/java/model/LotteryDrawingTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -15,40 +16,36 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 class LotteryDrawingTest {
 
     private final LotteryDrawing lotteryDrawing = new LotteryDrawing();
-    private final static LottoNumber one = new LottoNumber(1);
-    private final static LottoNumber two = new LottoNumber(2);
-    private final static LottoNumber three = new LottoNumber(3);
-    private final static LottoNumber four = new LottoNumber(4);
-    private final static LottoNumber five = new LottoNumber(5);
-    private final static LottoNumber six = new LottoNumber(6);
-    private final static LottoNumber seven = new LottoNumber(7);
-    private final static LottoNumber eight = new LottoNumber(8);
-    private final static LottoNumber nine = new LottoNumber(9);
-    private final static LottoNumber ten = new LottoNumber(10);
 
     @ParameterizedTest
     @DisplayName("등수를 추첨한다.")
     @MethodSource("provideLottoAndLotteryAndWinningResult")
-    void canDrawLottery(Lottery lottery, Map<Winning, Integer> expectedResult) {
+    void canDrawLottery(Lottery lottery, Winning winning) {
         // given
-        final Lotto winningNumbers = new Lotto(Arrays.asList(one, two, three, four, five, six));
+        final Lotto winningNumbers = createLotto(1, 2, 3, 4, 5, 6);
         final LottoNumber bonusNumber = new LottoNumber(7);
 
         // when
         final Map<Winning, Integer> winningResult = lotteryDrawing.drawLottery(winningNumbers, bonusNumber, lottery);
 
         // then
-        assertThat(winningResult).isEqualTo(expectedResult);
+        assertThat(winningResult.get(winning)).isEqualTo(1);
     }
 
     private static Stream<Arguments> provideLottoAndLotteryAndWinningResult() {
         return Stream.of(
-                Arguments.of(new Lottery(Collections.singletonList(new Lotto(Arrays.asList(one, two, three, four, five, six)))), Map.of(Winning.FIRST, 1)),
-                Arguments.of(new Lottery(Collections.singletonList(new Lotto(Arrays.asList(one, two, three, four, five, seven)))), Map.of(Winning.SECOND, 1)),
-                Arguments.of(new Lottery(Collections.singletonList(new Lotto(Arrays.asList(one, two, three, four, five, eight)))), Map.of(Winning.THIRD, 1)),
-                Arguments.of(new Lottery(Collections.singletonList(new Lotto(Arrays.asList(one, two, three, four, seven, eight)))), Map.of(Winning.FOURTH, 1)),
-                Arguments.of(new Lottery(Collections.singletonList(new Lotto(Arrays.asList(one, two, three, seven, eight, nine)))), Map.of(Winning.FIFTH, 1)),
-                Arguments.of(new Lottery(Collections.singletonList(new Lotto(Arrays.asList(one, two, seven, eight, nine, ten)))), Map.of(Winning.FAIL, 1))
+                Arguments.of(new Lottery(Collections.singletonList(createLotto(1, 2, 3, 4, 5, 6))), Winning.FIRST),
+                Arguments.of(new Lottery(Collections.singletonList(createLotto(1, 2, 3, 4, 5, 7))), Winning.SECOND),
+                Arguments.of(new Lottery(Collections.singletonList(createLotto(1, 2, 3, 4, 5, 8))), Winning.THIRD),
+                Arguments.of(new Lottery(Collections.singletonList(createLotto(1, 2, 3, 4, 7, 8))), Winning.FOURTH),
+                Arguments.of(new Lottery(Collections.singletonList(createLotto(1, 2, 3, 7, 8, 9))), Winning.FIFTH),
+                Arguments.of(new Lottery(Collections.singletonList(createLotto(1, 2, 7, 8, 9, 10))), Winning.FAIL)
         );
+    }
+
+    private static Lotto createLotto(int... numbers) {
+        return new Lotto(Arrays.stream(numbers)
+                .mapToObj(LottoNumber::new)
+                .collect(Collectors.toUnmodifiableList()));
     }
 }
